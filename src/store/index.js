c6536adb418eb8629e1d4c24b5df97d26c7b07f7
Mau Reyes <Mauricio.Reyes@rsmus.com>
2020-11-27 11:09:33 -0600

    global state management with combinereducers

--- src/store/index.js
+++ src/store/index.js
@@ -6,31 +6,25 @@ import {
   ageReducer,
 } from './reducers';
 
+const combineReducers = (...reducers) => (state = {}, action) => {
+  for (let i = 0; i < reducers.length; i++) state = reducers[i](state, action);
+  return state;
+};
+
+const initialState = Object.assign(initialUsername, initialAge);
+
 const StoreContext = createContext();
 
 export const StoreProvider = ({ children }) => {
-  /****************************************************************************
-   * Different way to combine reducers
-   * 1 have multiple reducers separate as useReducers
-   ***************************************************************************/
-  const [user, dispatchUser] = useReducer(userReducer, initialUsername);
-  const [age, dispatchAge] = useReducer(ageReducer, initialAge);
+  const [state, dispatch] = useReducer(
+    combineReducers(userReducer, ageReducer),
+    initialState
+  );
 
-  // const ProviderValue = useMemo(() => ({ state, dispatch }), [state, dispatch]);
-
-  let initState = {
-    ...user,
-    ...age,
-  };
-
-  const state = useMemo(() => ({ ...initState }), [initState]);
-
-  let dispatches = [dispatchUser, dispatchAge];
-
-  const dispatch = (action) => dispatches.forEach((fn) => fn(action));
+  const providerValue = useMemo(() => ({ state, dispatch }), [state, dispatch]);
 
   return (
-    <StoreContext.Provider value={{ state, dispatch }}>
+    <StoreContext.Provider value={providerValue}>
       {children}
     </StoreContext.Provider>
   );
